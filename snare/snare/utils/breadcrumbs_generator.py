e08fd0e3246c37eedb535f3a78be36016b4ebe1d
Veiglin <nicolaiveiglin@gmail.com>
2025-02-23 11:44:07 +0100

    trying to test the new args

--- snare/snare/utils/breadcrumbs_generator.py
+++ snare/snare/utils/breadcrumbs_generator.py
@@ -22,6 +22,9 @@ class BreadcrumbsGenerator:
         if self.breadcrumb in ['robots', 'robots.txt']:
             # Determine the path for robots.txt in the cloned pages directory.
             robots_path = os.path.join(self.page_dir, "robots.txt")
+
+            # Ensure the directory exists
+            os.makedirs(self.page_dir, exist_ok=True)
             
             # If the robots.txt file does not exist, create it with default content.
             if not os.path.exists(robots_path):
@@ -29,25 +32,33 @@ class BreadcrumbsGenerator:
                 with open(robots_path, "w") as f:
                     f.write(default_content)
             
-            # Compute the MD5 hash of the robots.txt file.
-            md5_hash = self._compute_md5(robots_path)
+            # Use the _make_filename_for_robots method to compute the MD5 hash.
+            #file_name, hash_name = self._make_filename_for_robots()
+            file_name = "/robots.txt"
+
+            hash_name = 'test_hash'  # Placeholder for the hash value.
             
-            # Update the meta dictionary with an entry for robots.txt.
-            # The key is formatted to match the style in meta.json (e.g., "/index.html").
-            self.meta["/robots.txt"] = {
-                "hash": md5_hash,
+            # Update the meta dictionary with the robots.txt entry.
+            self.meta[file_name] = {
+                "hash": hash_name,
                 "content_type": "text/plain"
             }
-            print("Breadcrumbs: Added robots.txt with hash {}".format(md5_hash))
+
+            print("Breadcrumbs: Added robots.txt with hash {}".format(hash_name))
         else:
             print("Breadcrumb type '{}' is not supported yet.".format(self.breadcrumb))
 
-    def _compute_md5(self, file_path):
+    def _make_filename_for_robots(self):
         """
-        Computes and returns the MD5 hash of the specified file.
+        Mimics the provided _make_filename method for generating an MD5 hash.
+        In this case, it computes the MD5 hash for '/robots.txt'.
         """
-        hash_md5 = hashlib.md5()
-        with open(file_path, "rb") as f:
-            for chunk in iter(lambda: f.read(4096), b""):
-                hash_md5.update(chunk)
-        return hash_md5.hexdigest()
+        file_name = "/robots.txt"
+        #if not file_name.startswith("/"):
+        #    file_name = "/" + file_name
+
+        # For robots.txt, we do not need to check for the special case of "/" since it's predefined.
+        m = hashlib.md5()
+        m.update(file_name.encode("utf-8"))
+        hash_name = m.hexdigest()
+        return file_name, hash_name
