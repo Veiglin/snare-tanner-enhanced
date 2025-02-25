8d4f4ac034b2150b84511cdbfe1c790d620006d8
Veiglin <nicolaiveiglin@gmail.com>
2025-02-25 09:18:00 +0100

    Update BreadcrumbsGenerator to support multiple breadcrumb types and enhance 404 page handling

--- snare/snare/utils/breadcrumbs_generator.py
+++ snare/snare/utils/breadcrumbs_generator.py
@@ -5,35 +5,44 @@ import json
 from snare.utils.snare_helpers import print_color
 
 class BreadcrumbsGenerator:
-    def __init__(self, page_dir, meta, breadcrumb='robots'):
+    def __init__(self, page_dir, meta, breadcrumb):
         """
         Initializes the breadcrumbs generator.
         
         :param page_dir: The directory where cloned pages are stored.
         :param meta: The meta dictionary (parsed from meta.json).
-        :param breadcrumb: The type of breadcrumb to generate. Currently supports 'robots' only.
+        :param breadcrumb: The type of breadcrumb to generate.
         """
         self.page_dir = page_dir
         self.meta = meta
         self.breadcrumb = breadcrumb
-
+    
     def generate_breadcrumbs(self):
         """
-        Generates breadcrumbs for the given type. For now, it creates/updates a robots.txt file.
-        The file's MD5 hash is computed and added to the meta dictionary.
+        Generates breadcrumbs for the given types.
         """
-        if self.breadcrumb in ['robots']:
-            # Use the _make_filename_for_robots method to compute the MD5 hash.
-            file_name = "robots.txt"
-            hash_name = self.make_filename(file_name)
+        for breadcrumb in self.breadcrumb:
+            if breadcrumb == 'robots':
+                self.generate_robots_breadcrumb()
+            elif breadcrumb == '404_page':
+                self.generate_404_breadcrumb()
+            else:
+                print_color("Breadcrumb type '{}' is not supported yet.".format(breadcrumb), "WARNING")
 
-            robots_path = os.path.join(self.page_dir, hash_name)
-            # If the robots.txt file does not exist, create it with default content.
-            if not os.path.exists(robots_path):
-                default_content = "User-agent: *\nDisallow:"  # You can adjust the content as needed.
-                with open(robots_path, "w") as f:
-                    f.write(default_content)
-            
+    def generate_robots_breadcrumb(self):
+        """
+        Generates a robots.txt breadcrumb.
+        """
+        file_name = "robots.txt"
+        hash_name = self.make_filename(file_name)
+        robots_path = os.path.join(self.page_dir, hash_name)
+        
+        # If the robots.txt file does not exist, create it with default content.
+        if not os.path.exists(robots_path):
+            default_content = "User-agent: *\nDisallow:"  # You can adjust the content as needed.
+            with open(robots_path, "w") as f:
+                f.write(default_content)
+        
             abs_url = "/robots.txt" 
             self.meta[abs_url] = {
                 "hash": hash_name,
@@ -46,32 +55,36 @@ class BreadcrumbsGenerator:
                 json.dump(self.meta, meta_file, indent=4)
 
             print_color("Breadcrumbing: Added robots.txt as breadcrumb with hash '{}'".format(hash_name))
+        else:
+            print_color("Breadcrumbing: robots.txt already exists.")
 
-        if self.breadcrumb in ['404_page']:
-
-            # find the 404 page hash in the meta dictionary
-            for key, val in self.meta.items():
-                if "404" in key:
-                    abs_url = key
-                    hash_name = val["hash"]
-                    break
+    def generate_404_breadcrumb(self):
+        """
+        Generates a 404 page breadcrumb.
+        """
+        # find the 404 page hash in the meta dictionary
+        for key, val in self.meta.items():
+            if "404" in key:
+                hash_name = val["hash"]
+                break
             
-            # go to the hash file name and change the content of the html file
-            html_path = os.path.join(self.page_dir, hash_name)
-            with open(html_path, "r") as f:
-                html_content = f.read()
-                # change the content of the 404 page as needed by adding a message
-                msg = "<p>Try accessing test, test, test for more information.</p>"
-                html_content = html_content.replace("</body>", msg + "</body>")
+        # go to the hash file name and change the content of the html file
+        html_path = os.path.join(self.page_dir, hash_name)
+        with open(html_path, "r") as f:
+            html_content = f.read()
+            # check if the 404 page already has a message
+            if "Try accessing" in html_content:
+                print_color("Breadcrumbing: 404 page already has a custom message.")
+                return None
+            # change the content of the 404 page as needed by adding a message
+            msg = "<p>Try accessing test, test, test for more information.</p>"
+            html_content = html_content.replace("</body>", msg + "</body>")
 
-            # save the new content in the hash file
-            with open(html_path, "w") as f:
-                f.write(html_content)
+        # save the new content in the hash file
+        with open(html_path, "w") as f:
+            f.write(html_content)
 
-            print_color("Breacrumbing: Updated 404 page with message '{}'".format(msg))
-
-        #else:
-        #    print_color("Breadcrumbing: Breadcrumb type '{}' is not supported yet.".format(self.breadcrumb), "WARNING")
+        print_color("Breacrumbing: Updated 404 page with message '{}'".format(msg))
 
     @staticmethod
     def make_filename(file_name):
