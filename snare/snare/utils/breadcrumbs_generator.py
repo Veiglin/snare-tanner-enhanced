e3070c4d4158d5d4e0649379339315a5d1ea8104
Veiglin <nicolaiveiglin@gmail.com>
2025-02-25 09:00:22 +0100

    Update BreadcrumbsGenerator to support 404_page and modify robots.txt handling

--- snare/snare/utils/breadcrumbs_generator.py
+++ snare/snare/utils/breadcrumbs_generator.py
@@ -1,9 +1,8 @@
 import os
 import hashlib
-import logging
 import json
 
-from snare.utils.snare_helpers import check_privileges, print_color
+from snare.utils.snare_helpers import print_color
 
 class BreadcrumbsGenerator:
     def __init__(self, page_dir, meta, breadcrumb='robots'):
@@ -24,22 +23,19 @@ class BreadcrumbsGenerator:
         The file's MD5 hash is computed and added to the meta dictionary.
         """
         if self.breadcrumb in ['robots']:
-            # Determine the path for robots.txt in the cloned pages directory.
-            robots_path = os.path.join(self.page_dir, "robots")
-            
+            # Use the _make_filename_for_robots method to compute the MD5 hash.
+            file_name = "robots.txt"
+            hash_name = self.make_filename(file_name)
+
+            robots_path = os.path.join(self.page_dir, hash_name)
             # If the robots.txt file does not exist, create it with default content.
             if not os.path.exists(robots_path):
                 default_content = "User-agent: *\nDisallow:"  # You can adjust the content as needed.
                 with open(robots_path, "w") as f:
                     f.write(default_content)
             
-            # Use the _make_filename_for_robots method to compute the MD5 hash.
-            #file_name, hash_name = self._make_filename_for_robots()
-            file_name = "/robots.txt"  # Placeholder for the file name.
-            hash_name = 'robots'  # Placeholder for the hash value.
-            
-            # Update the meta dictionary with the robots.txt entry.
-            self.meta[file_name] = {
+            abs_url = "/robots.txt" 
+            self.meta[abs_url] = {
                 "hash": hash_name,
                 "content_type": "text/plain"
             }
@@ -49,18 +45,39 @@ class BreadcrumbsGenerator:
             with open(meta_json_path, "w") as meta_file:
                 json.dump(self.meta, meta_file, indent=4)
 
-            print_color("Added robots.txt as breadcrumb with hash '{}'".format(hash_name))
+            print_color("Breadcrumbing: Added robots.txt as breadcrumb with hash '{}'".format(hash_name))
 
-        else:
-            print_color("Breadcrumb type '{}' is not supported yet.".format(self.breadcrumb), "WARNING")
+        if self.breadcrumb in ['404_page']:
 
+            # find the 404 page hash in the meta dictionary
+            for key, val in self.meta.items():
+                if "404" in key:
+                    abs_url = key
+                    hash_name = val["hash"]
+                    break
+            
+            # go to the hash file name and change the content of the html file
+            html_path = os.path.join(self.page_dir, hash_name)
+            with open(html_path, "r") as f:
+                html_content = f.read()
+                # change the content of the 404 page as needed by adding a message
+                msg = "<p>Try accessing test, test, test for more information.</p>"
+                html_content = html_content.replace("</body>", msg + "</body>")
 
-    def _make_filename(self, path):
+            # save the new content in the hash file
+            with open(html_path, "w") as f:
+                f.write(html_content)
+
+            print_color("Breacrumbing: Updated 404 page with message '{}'".format(msg))
+
+        #else:
+        #    print_color("Breadcrumbing: Breadcrumb type '{}' is not supported yet.".format(self.breadcrumb), "WARNING")
+
+    @staticmethod
+    def make_filename(file_name):
         # Compute the MD5 hash of the content
-        with open(path, "r") as f:
-            content = f.read()
         m = hashlib.md5()  
-        m.update(content.encode("utf-8"))
+        m.update(file_name.encode("utf-8"))
         hash_name = m.hexdigest()
 
         return hash_name
