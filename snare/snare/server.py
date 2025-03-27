07f7cb5777b14478035058eeefac6760d06528fd
Veiglin <nicolaiveiglin@gmail.com>
2025-03-27 07:12:50 +0100

    Add SSL support to HttpRequestHandler for secure connections

--- snare/snare/server.py
+++ snare/snare/server.py
@@ -2,6 +2,7 @@ import logging
 import aiohttp
 import aiohttp_jinja2
 import jinja2
+import ssl
 
 from aiohttp import web
 from aiohttp.web import StaticResource as StaticRoute
@@ -88,6 +89,14 @@ class HttpRequestHandler:
 
         self.runner = web.AppRunner(app)
         await self.runner.setup()
+
+        # Create an SSL context with your certificate and key.
+        ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        ssl_context.load_cert_chain(
+            certfile='/etc/letsencrypt/live/smartgadgetstore.live/fullchain.pem',
+            keyfile='/etc/letsencrypt/live/smartgadgetstore.live/privkey.pem'
+        )
+
         site = web.TCPSite(self.runner, self.run_args.host_ip, self.run_args.port)
 
         await site.start()
