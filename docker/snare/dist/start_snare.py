e6725022d0b85a1c84e166c91d5db693f4a07e6e
cpalox <132818294+cpalox@users.noreply.github.com>
2025-04-17 12:27:36 +0200

    Updated webpages (#25)
    * New Templates + Docker File CMD randomized
    
    * Fixed meta for new webpages
    
    * Small extra fixes for metajson
    
    * Loginphp style fixed
    
    * Update content types in meta.json files for CSS and JS assets
    
    * Some files needed to be updated
    
    * Old Webpages deleted
    
    * Enable HONEYTOKEN feature in config.yaml

---
+++ docker/snare/dist/start_snare.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import subprocess
+import random
+import time
+import requests
+
+dirs = [
+    "smartgadgetstore1",
+    "smartgadgetstore2",
+    "smartgadgetstore3",
+    "smartgadgetstore4"
+]
+
+selected_dir = random.choice(dirs)
+
+cmd = [
+    "snare",
+    "--tanner", "tanner",
+    "--debug", "true",
+    "--auto-update", "false",
+    "--host-ip", "0.0.0.0",
+    "--port", "443",
+    "--page-dir", selected_dir,
+    "--breadcrumbs", "robots", "robots", "404_page", "html_comments"
+]
+
+
+def wait_for_tanner(url="http://tanner:8090/version", retries=10):
+    for i in range(retries):
+        try:
+            r = requests.get(url, timeout=2)
+            if r.ok:
+                return
+        except Exception:
+            time.sleep(2)
+    raise RuntimeError("Tanner service did not start in time.")
+
+wait_for_tanner()
+# then start snare
+
+
+subprocess.run(cmd)
+
