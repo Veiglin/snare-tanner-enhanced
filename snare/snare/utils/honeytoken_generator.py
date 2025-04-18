3955dc8e116fecbdfd872c6401912e9e3dc4b5f5
Veiglin <nicolaiveiglin@gmail.com>
2025-04-18 10:57:31 +0200

    add prod docker

--- snare/snare/utils/honeytoken_generator.py
+++ snare/snare/utils/honeytoken_generator.py
@@ -23,7 +23,7 @@ class HoneytokensGenerator:
         self.api_endpoint = SnareConfig.get("HONEYTOKEN", "API-ENDPOINT")
         self.api_key = SnareConfig.get("HONEYTOKEN", "API-KEY")
         self.llm_parameters = SnareConfig.get("HONEYTOKEN", "LLM-PARAMETERS")
-        self.webhook_url = "  
+        self.webhook_url = "http:// :5003/webhook"
         self.marker = "__honeypot_honeytokens_marker__"
         # 🔒 Hardcoded log directory
         self.track_dir = os.path.join("/opt/snare", "honeytokens")
